8aa1da9e111bb3f16a082bef93fc01865c9a2821
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-07-27 22:18:41 +0900

    update

--- gdriveClient.py
+++ gdriveClient.py
@@ -27,14 +27,60 @@ class ClassApp:
         __class__._log = get_logger(__class__.__name__, self._dbg)
         self._log.debug('')
 
-        self._gauth = GoogleAuth()
+        try:
+            self._gauth = GoogleAuth()
+        except Exception as e:
+            self._debug('%s:%s', type(e), e)
+            
         self._gauth.LocalWebserverAuth()
 
-        self._drive = GoogleDrive(self._gauth)
+        self._drv = GoogleDrive(self._gauth)
 
     def main(self):
         self._log.debug('')
-        
+
+        top_id = self.get_folder_id('ScanSnap')
+        if len(top_id) == 0:
+            self._log.error('???')
+            return
+
+        self.get_list(top_id)
+
+    def get_folder_id(self, folder_name):
+        """
+        get_folder_id
+        """
+        self._log.debug('folder_name=%s', folder_name)
+
+        ids = self._drv.ListFile({'q': "title='%s'" % folder_name}).GetList()
+        if len(ids) != 1:
+            self._log.error('???')
+            return ''
+
+        return ids[0]['id']
+
+    def get_list(self, id):
+        """
+        get_list
+        """
+        self._log.debug('id=%s', id)
+
+        try:
+            flist = self._drv.ListFile({
+                'q':
+                '"%s" in parents and trashed = false' % id
+            }).GetList()
+        except Exception as ex:
+            self._debug('%s:%s', type(ex), ex)
+        if flist == []:
+            self._log.error('???')
+            return []
+
+        for f in flist:
+            print('%s (%s)' % (f['title'], f['mimeType']))
+
+            if f['mimeType'] == 'application/vnd.google-apps.folder':
+                self.get_list(f['id'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
@@ -53,4 +99,3 @@ def main(debug):
 
 if __name__ == "__main__":
     main()
-
