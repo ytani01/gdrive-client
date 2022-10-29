bff4b0bff0f76eafc9a11e6df7d68649274873d9
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2022-10-30 02:27:55 +0900

    UPD: gdriv_check_folder.py: 0.1.1

--- gdrv_check_folder.py
+++ gdrv_check_folder.py
@@ -18,12 +18,13 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 MYNAME = 'gdrv_check_folder'
-VERSION = '0.1.0'
-
+VERSION = '0.1.1'
 
 class Gdrv:
     """ Gdrv """
     _log = get_logger(__name__, False)
+
+    DEF_LOOP_INTERVAL = 15
 
     def __init__(self, debug=WARNING):
         self._dbg = debug
@@ -43,21 +44,25 @@ class Gdrv:
     def check_folder(self, folder, cmd_add='', cmd_remove=''):
         self._log.debug('folder=%a', folder)
 
+        # ts_str: Time Stamp String: 'yyyy-mm-dd HH:MM:SS'
         ts_str = '%s' % datetime.datetime.now()
         ts_str = ts_str[0:19]
 
+        # q_str: Querry string for gdrive
         q_str = "'%s' in parents" % folder
         q_str += ' and trashed=false'
         self._log.debug('q_str=%a', q_str)
         q = {'q': '%s' % q_str}
         self._log.debug('q=%s', q)
 
+        # flist: current file list from gdrive
         flist = self._drv.ListFile(q).GetList()
         self._log.debug('num of files=%d', len(flist))
 
         dec_count = 0
         inc_count = 0
 
+        # title_list: current title list
         title_list = []
         for f in flist:
             title_list.append(f['title'])
@@ -92,7 +97,8 @@ class Gdrv:
 
         self._flist = new_flist
 
-    def loop(self, folder, interval=15, cmd_add='', cmd_remove=''):
+    def loop(self, folder, interval=DEF_LOOP_INTERVAL,
+             cmd_add='', cmd_remove=''):
         while True:
             self.check_folder(folder, cmd_add, cmd_remove)
             time.sleep(interval)
@@ -101,8 +107,9 @@ class Gdrv:
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('folder')
-@click.option('--interval', '-s', 'interval', type=int, default=5,
-              help='interval sec')
+@click.option('--interval', '-s', 'interval', type=int,
+              default=Gdrv.DEF_LOOP_INTERVAL,
+              help='interval sec (default: %s sec)' % Gdrv.DEF_LOOP_INTERVAL)
 @click.option('--cmd_add', '-a', 'cmd_add', type=str, default='',
               help='command (add)')
 @click.option('--cmd_remove', '-r', 'cmd_remove', type=str, default='',
